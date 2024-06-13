74c5e10a05a6109347bfe7d2280e637c02ae42bb
Kuroyukimaru <oranggabut712@gmail.com>
2024-06-13 10:48:38 +0700

    Connstruktor for the node class

--- BinarySearchTree_0151/BinarySearchTree_0151.cpp
+++ BinarySearchTree_0151/BinarySearchTree_0151.cpp
@@ -5,6 +5,15 @@ using namespace std;
 class Node {
 public:
     string info;
+    Node* leftchild; 
+    Node* rightchild; 
+
+    // Connstruktor for the node class
+    Node(string i, Node* l, Node* r) {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
 };
 int main()
 {
