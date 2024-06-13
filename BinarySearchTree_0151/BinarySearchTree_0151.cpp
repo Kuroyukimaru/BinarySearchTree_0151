b7dcb038cc11d5360aa8357c5332ea3ed400822a
Kuroyukimaru <oranggabut712@gmail.com>
2024-06-13 11:02:49 +0700

    Temukan node yang akan menjadi induk dari node yang akan dicari

--- BinarySearchTree_0151/BinarySearchTree_0151.cpp
+++ BinarySearchTree_0151/BinarySearchTree_0151.cpp
@@ -29,6 +29,9 @@ public:
         newNode->leftchild = nullptr;   // Make the left child of the  new node point to NULL
         newNode->rightchild = nullptr;  // Make the right child of the new node point to NULL
 
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // Locate the node which will be the parent of the node to be insearch
     }
 };
 int main()
