09ea2049da491ca96dd9151e2d865ed8941ee78b
Kuroyukimaru <oranggabut712@gmail.com>
2024-06-13 10:55:32 +0700

    Alokasikan memori untuk node baru

--- BinarySearchTree_0151/BinarySearchTree_0151.cpp
+++ BinarySearchTree_0151/BinarySearchTree_0151.cpp
@@ -23,7 +23,10 @@ public:
         ROOT = nullptr; // Initializing Root to null
     }
 
-    void insert(string element) // Insert a node in the binary search tree
+    void insert(string element) { // Insert a node in the binary search tree
+        Node* newNode = new Node(element, nullptr, nullptr);    // Allocate memory for the new node
+
+    }
 };
 int main()
 {
